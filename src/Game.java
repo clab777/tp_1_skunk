b2dcb5e2a5f2631be0104b7522c9eba2204fa3e3
sleng0022 <leng.sinith001@gmail.com>
2018-02-25 17:23:28 -0600

    add player receiving 2 skunks.

--- src/Game.java
+++ src/Game.java
@@ -13,6 +13,11 @@ public class Game
 		chips_total += 1;
 	}
 	
+	public void recieves2Skunk()
+	{
+		chips_total += 4;
+	}
+	
 	public void addChip(int num)
 	{
 		chips_total += num;
--- test/GameTest.java
+++ test/GameTest.java
@@ -28,5 +28,15 @@ public class GameTest {
 		assertEquals(1, game1.getTotalChips());
 	}
 	
+	@Test
+	public void testPlayerGet2Skunk()
+	{
+		Game game1 = new Game();
+		Player p1 = new Player(1, "Sinith");
+		game1.recieves2Skunk();
+		p1.drawsChip(4);
+		assertEquals(46, p1.getTotalChipLeft());
+		assertEquals(4, game1.getTotalChips());
+	}
 
 }
