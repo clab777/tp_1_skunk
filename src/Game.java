db59113920780a97b00a48763ac53be46b90ba73
sleng002 <leng.sinith001@gmail.com>
2018-03-07 00:33:42 -0600

    prompt user to reroll the dice

--- src/Game.java
+++ src/Game.java
@@ -16,6 +16,9 @@ public class Game
 	public void play()
 	{
 		Scanner reader = new Scanner(System.in);
+		StdOut.println("-----------------------------------------------------------------------------------------");
+		StdOut.println("Welcome to the game of Skunk, Let's play some game!");
+		StdOut.println("-----------------------------------------------------------------------------------------");
 		System.out.print("Enter number of players:");		
 		num_players = reader.nextInt();
 		if(num_players == 0)
@@ -38,14 +41,35 @@ public class Game
 			Player player = new Player(i, playerID);
 			players.add(player);
 		}
-		reader.close();
 	}
 	
-	public void startTheGame()
+	private void startTheGame()
 	{
+		boolean reroll = false;
 		
+		reroll = this.askForReroll();
+		if(reroll == true)
+		{
+			System.out.print("Winner");
+		}
 	}
     
+	private boolean askForReroll()
+	{
+		String respond;
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Would you like to reroll?(Y/N)");
+		respond = reader.nextLine();
+		reader.close();
+		if(respond.equals("Y") || respond.equals("y"))
+		{
+			return true;
+		}else  
+		{
+			return false;
+		}
+	}
+	
 	public int getNumberofPlayers()
 	{
 		return num_players;
